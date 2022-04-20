9248c45029ba185e268141f78445ab83ef1042f4
Sean McKinney <seanamckinney8@gmail.com>
2022-04-20 10:27:40 -0500

    added data layer and moved connection string from data.ef to configs

---
+++ JazAndSeanSATProject.DATA.EF/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JazAndSeanSATProject.DATA.EF
+{
+    public class Class1
+    {
+    }//end class
+}//end namespace
